0c4350922f53d3e87ba34cad6c133c9c4ad45b40
SogCheonBul <jy6443@naver.com>
2024-11-11 15:29:27 +0900

    Test

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM ubuntu:latest
-LABEL authors="junyoung"
-
-ENTRYPOINT ["top", "-b"]
--- src/main/java/com/test/demo/controller/TestController.java
+++ src/main/java/com/test/demo/controller/TestController.java
@@ -1,2 +1,17 @@
-package com.test.demo.controller;public class TestController {
+package com.test.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/test")
+public class TestController {
+    @GetMapping("/hello")
+    public ResponseEntity<Object> testApi() {
+        String result = "API 통신에 성공했습니다.";
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 }
