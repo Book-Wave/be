11a9534560a2eb69521a258a81557af3216df0e4
platypus3036 <akakehcn@gmail.com>
2024-11-15 15:41:33 +0900

    ADD: RootConfig, WebConfig, WebSocketConfig 추가

--- src/main/java/com/test/demo/DemoApplication.java
+++ src/main/java/com/test/demo/DemoApplication.java
@@ -1,11 +1,13 @@
 package com.test.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
+//  서버가 실행되면 JRE가 이 클래스의 메서드를 찾아서 자동 실행이 된다.
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
---
+++ src/main/java/com/test/demo/config/WebConfig.java
@@ -0,0 +1,32 @@
+package com.test.demo.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        System.out.println("WebConfigurer addCorsMappings ~");
+        // 접속 클라이언트를 허가(Restful)
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")  // Vue의 개발 서버 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+
+}
