439ab691a41b2b702c0837d9c4b12f9c61591161
platypus3036 <akakehcn@gmail.com>
2025-01-13 23:55:41 +0900

    Temp

--- src/main/java/com/test/demo/controller/ShopController.java
+++ src/main/java/com/test/demo/controller/ShopController.java
@@ -1,4 +1,28 @@
 package com.test.demo.controller;
 
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/book/shop")
 public class ShopController {
+
+
+    @GetMapping("/{username}/items")
+    public static ResponseEntity<> getUserItems(@PathVariable("username") String username) {
+
+    }
+
+
+
+
 }
